3a089adef9ebee9dd68f48d2eb433575c576a934
opencogmind <autoagi.university@gmail.com>
2023-11-03 03:29:36 +0500

    change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'OpenCogMind',
-  tagline: 'Follow path from AI to AGI',
+  tagline: 'Follow this path from AI to AGI',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
