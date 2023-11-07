a62b62ad57a7b03d2b630baad5a8d435a3c1a129
opencogmind <autoagi.university@gmail.com>
2023-11-07 15:58:08 +0500

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
     
     <header className={clsx('hero ', styles.heroBanner)} style={heroStyle}>
       <div className="container">
-        <h1 style={{color: 'darkgrey' }} className="hero__title">{siteConfig.title}</h1>
+        <h1 style={{color: 'darkgrey', textAlign:'center' }} className="hero__title">{siteConfig.title}</h1>
         <p style={{color: 'darkgrey' }} className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
